3d16d8a35128edf0f4e07839ddc264c94c182ae6
Amatakaze <AaronDichoso17@gmail.com>
2025-12-01 11:05:32 +0800

    A

--- Networks/PopNet/Code/utils/data.py
+++ Networks/PopNet/Code/utils/data.py
@@ -404,7 +404,7 @@ def get_loader_var_unlabel(image_root, gt_root,depth_root, batchsize, trainsize,
 class test_dataset:
     def __init__(self, image_root, gt_root,depth_root, testsize):
         self.testsize = testsize
-        self.images = [image_root + f for f in os.listdir(image_root) if f.endswith('.jpg')]
+        self.images = [image_root + f for f in os.listdir(image_root) if f.endswith('.jpg') or f.endswith('.png')]
         self.gts = [gt_root + f for f in os.listdir(gt_root) if f.endswith('.jpg')
                        or f.endswith('.png')]
         self.depths=[depth_root + f for f in os.listdir(depth_root) if f.endswith('.bmp')
